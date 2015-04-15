4c5c3732d3c51fb43a22870d7695775a32b0c149
tkmagesh <tkmagesh77@gmail.com>
2015-04-15 16:09:42 +0530

    Day-1 Complet

--- Day-01/04-webServer/server.js
+++ Day-01/04-webServer/server.js
@@ -1,25 +1,41 @@
 var http = require('http'),
     path = require('path'),
-    fs = require('fs');
+    fs = require('fs'),
+    url = require('url'),
+    calculator = require('./calculator');
+
+var statisResourceExtns = ['.html','.css','.js','.txt','.jpg','.png','.json','.ico'];
+
+function isStatic(resource){
+    var resourceExtn = path.extname(resource);
+    return statisResourceExtns.indexOf(resourceExtn) !== -1;
+}
 
 var server = http.createServer(function(req, res){
-    var resourcePath = path.join(__dirname, req.url);
-    if (!fs.existsSync(resourcePath)){
+    var urlData = url.parse(req.url, true);
+    if (isStatic(urlData.pathname)){
+        var resourcePath = path.join(__dirname, urlData.pathname);
+        if (!fs.existsSync(resourcePath)){
+            res.statusCode = 404;
+            res.end();
+            return;
+        }
+        fs.createReadStream(resourcePath, {encoding : 'utf8'}).pipe(res);
+    } else if (urlData.pathname === '/calculator'){
+        var data = {
+            op : urlData.query.op,
+            n1 : parseInt(urlData.query.n1),
+            n2 : parseInt(urlData.query.n2)
+        };
+        var result = calculator[data.op](data.n1,data.n2);
+        res.write(result.toString());
+        res.end();
+    } else {
         res.statusCode = 404;
         res.end();
-        return;
     }
-    var stream = fs.createReadStream(resourcePath, {encoding : 'utf8'});
-   /* stream.on('data', function(chunk){
-        res.write(chunk);
-    });
-    stream.on('end', function(){
-        res.end();
-    });*/
-    stream.pipe(res);
-
 });
 server.listen(9090);
-console.log('server listening on port 9090');
+console.log('server listening on port 9090..');
 
 
