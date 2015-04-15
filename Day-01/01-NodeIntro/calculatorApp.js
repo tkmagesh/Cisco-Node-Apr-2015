313c0ff0e7fea0c9a4ab6f55479e394efbb95d59
tkmagesh <tkmagesh77@gmail.com>
2015-04-15 11:48:08 +0530

    Basics

---
+++ Day-01/01-NodeIntro/calculatorApp.js
@@ -0,0 +1,7 @@
+var n1 = 100,
+    n2 = 200;
+
+console.log("add = ", calculator.add(n1,n2));
+console.log("subtract = ", calculator.subtract(n1,n2));
+console.log("divide = ", calculator.divide(n1,n2));
+console.log("multiply = ", calculator.multiply(n1,n2));
---
+++ Day-01/01-NodeIntro/intro.js
@@ -0,0 +1,1 @@
+console.log("Hi there!, welcome to node");
