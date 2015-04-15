88d7afc4f6ec9f07166101f5bb00bd44a9d866e7
tkmagesh <tkmagesh77@gmail.com>
2015-04-15 12:55:56 +0530

    web server - part - 2

--- Day-01/04-webServer/server.js
+++ Day-01/04-webServer/server.js
@@ -1,7 +1,20 @@
-var http = require('http');
+var http = require('http'),
+    path = require('path'),
+    fs = require('fs');
+
 var server = http.createServer(function(req, res){
-    res.write('A new connection is estalished for '+ req.url);
-    res.end();
+    var resourcePath = path.join(__dirname, req.url);
+    res.write("write starts at " + new Date().toString());
+    fs.readFile(resourcePath, {encoding : 'utf8'}, function(err, data){
+        if (!err){
+            res.write(data);
+            fs.write("write ends at " + new Date().toString());
+            res.end();
+        } else {
+            res.statusCode = 404;
+            res.end();
+        }
+    });
 });
 server.listen(9090);
 console.log('server listening on port 9090');
